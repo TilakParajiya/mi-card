9cbbc2f10eee165020db213fe4569b810cd4848e
Tilak Parajiya <tilak.parajiya@tudip.com>
2023-04-11 14:54:06 +0530

    Some other changes and added few other links

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:mailto/mailto.dart';
 
 void main() {
   runApp(
@@ -7,6 +9,9 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  String linkedin_url = 'https://www.linkedin.com/in/tilak-parajiya-15a8a019a';
+  String github_url = 'https://github.com/TilakParajiya';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -72,28 +77,56 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: ListTile(
-                  // mainAxisAlignment: MainAxisAlignment.center,
-                  // mainAxisSize: MainAxisSize.min,
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.teal,
+              InkWell(
+                onTap: _launchMailto,
+                child: Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
                   ),
-
-                  title: Text(
-                    'tilakparajiya1@gmail.com',
-                    style: TextStyle(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'tilakparajiya1@gmail.com',
+                      style: TextStyle(
                         fontSize: 19.0,
                         fontFamily: 'PatuaOne',
-                        color: Colors.teal.shade900),
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
                   ),
                 ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: _launchGitURL,
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        // color: Colors.white,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child:Image(image: NetworkImage('https://w7.pngwing.com/pngs/914/758/png-transparent-github-social-media-computer-icons-logo-android-github-logo-computer-wallpaper-banner-thumbnail.png'),width: 40,height: 40,),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: _launchLnkdnURL,
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child:Image(image: NetworkImage('https://cdn-icons-png.flaticon.com/512/174/174857.png'),width: 40,height: 40,),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
@@ -101,4 +134,18 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+
+  void _launchGitURL() async => await canLaunchUrlString(github_url)
+        ? await launchUrlString(github_url)
+        : throw 'Could not launch $github_url';
+  void _launchLnkdnURL() async => await canLaunchUrlString(linkedin_url)
+        ? await launchUrlString(linkedin_url)
+        : throw 'Could not launch $linkedin_url';
+  final mailtoLink = Mailto(
+    to: ['tilakparajiya1@gmail.com'],
+  );
+  void _launchMailto() async => await canLaunchUrlString('$mailtoLink')
+      ? await launchUrlString('$mailtoLink')
+      : throw 'Could not launch $mailtoLink';
+
 }
